5f6101403cf1b2bcc0d186d91c5a7a8547d8a1ce
João Fernando Garcia de Souza <joaofernandogarciadesouza@hotmail.com>
2019-06-11 00:11:29 -0300

    first commit

---
+++ Projeto/php/connection.php
@@ -0,0 +1,14 @@
+<?php
+$servername = "sql301.unaux.com";
+$database = "unaux_23707723_Consus";
+$username = "unaux_23707723";
+$password = "pgo00rrnta";
+// Cria conexão
+$con = mysqli_connect($servername, $username, $password, $database);
+mysqli_set_charset($con,"utf8");
+// Checa a conexão
+if (!$con) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
---
+++ Projeto/php/inadimp.php
@@ -0,0 +1,29 @@
+<?php
+include_once("connection.php");   //estabelece conexão
+
+header ('Content-type: text/html; charset=utf-8');
+
+if(isset($_POST["id"]))     //verifica se a variável existe, ou tem valor diferente de null
+{   // select que vai consultar no banco
+    $query = "SELECT d.*, c.debtor_name, s.store_social_name
+            FROM Debt d
+            INNER JOIN Debtor c ON d.debtor_id = c.debtor_cpf
+            INNER JOIN Store s ON d.store_id = s.store_id
+            WHERE c.debtor_cpf = '".addslashes($_POST["id"])."'
+            ORDER BY d.debt_id";
+    //fim do select----------------------
+    $dados = mysqli_query($con, $query) or die($mysqli->error); //envia para o banco a query
+
+    $recebeJson = array();  //estabelece como um array
+    while($dado = mysqli_fetch_assoc($dados)){
+        $data["nome"] = $dado["debtor_name"];  
+        $data["loja"] = $dado["store_social_name"];
+        $data["obs"] = $dado["debt_observation"];
+        $data["valor"] = $dado["debt_value"];
+        array_push($recebeJson, $data);             //pega os dados de data e passa para recebeJson
+    }
+    echo json_encode($recebeJson, JSON_FORCE_OBJECT); //escreve na pagina um objeto json
+}
+// tira o resultado da busca da memória
+mysqli_close($con);
+?>
